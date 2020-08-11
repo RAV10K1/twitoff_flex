d57dfd8a3a25f6921f6fc88cfbe8a7123c619307
RAV10K1 <ravindra.tennekone@gmail.com>
2020-08-10 18:20:53 -0700

    added html and tweeoy

--- web_app/app.py
+++ web_app/app.py
@@ -1,13 +1,14 @@
-from flask import Flask
-from .db_models import db
+from flask import Flask, render_template
+from .db_models import db, User
 
 def create_app():
     '''Create and configure an instance of the Flask application'''
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tweet_repo.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
     
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff!'
+        return render_template('base.html', title="Home", users=User.query.all())
     return app
--- web_app/db_models.py
+++ web_app/db_models.py
@@ -2,6 +2,7 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import backref
+from sqlalchemy.sql.schema import ForeignKey
 
 db = SQLAlchemy()
 
@@ -9,6 +10,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     follower_count = db.Column(db.String(120), unique=True, nullable=False)
+    newest_tweet_id = db.Column(db.BigInteger, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,6 +19,8 @@ class User(db.Model):
 class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Unicode(300))
+    embedding = db.Column(db.PickleType, nullable=False)
+    user_id = db.Column(db.BigInteger, db.ForeignKey('user.id'), nullable=False)
     # user = db.Relationship('User', backref=db.backref('text', lazy=True))
     
     def __repr__(self):
